ffaaaa6ba0c9c112989ed7feb8017c9e7d8f9aa4
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-10-27 12:22:39 -0500

    Initual AboutUs Files configuration

---
+++ client/src/components/home/About/AboutUs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../../styles/AboutUs.module.css"
+
+
+const AboutUs = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default AboutUs;
