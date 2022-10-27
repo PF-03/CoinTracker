25def955d57d19e56c5cb1e0732ea631f0049fce
milagros vetcher <milivetchergmail.com>
2022-10-26 22:23:42 -0300

    busqueda funciona perfecta

--- server/src/routes/middleware/activs.ts
+++ server/src/routes/middleware/activs.ts
@@ -5,10 +5,20 @@ import { getActivos } from '../../controllers/getActivos';
 const activos = express.Router();
 
  
-activos.get('/', async (_req, res)=> {
-    const activos = await getActivos();
-    console.log(activos)
-    res.send(activos);
+activos.get('/', async (req, res)=> {
+    const {name} = req.query;
+    const _activos = await getActivos();
+    
+    if(!name){
+        res.send(_activos)
+    }else{
+        let foundName = _activos.filter((e:any) => e.name.toLowerCase().includes(name.toString().toLowerCase()));
+        foundName.length ?
+        res.status(200).send(foundName) :
+        res.status(404).json({message:'Activo not found :('})
+    }
+
+    
 })
 
 activos.get('/:filter', async(req, res)=>{
