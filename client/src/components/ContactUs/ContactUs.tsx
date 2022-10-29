02b33307c8ff51a68910ff5bb049e0e4a7bf60a3
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-10-29 13:38:25 -0500

    PF-13 <State configuration and Styles>

--- client/src/components/ContactUs/ContactUs.tsx
+++ client/src/components/ContactUs/ContactUs.tsx
@@ -1,23 +1,63 @@
 import React from "react";
 import styles from "../styles/ContactUs.module.css";
 import Button from "../styles/button";
+import Telefono from "../../assets/telefono.png";
 const ContactUs = () => {
+  const [state, setState] = React.useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const HandleChange = (e: any) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit ", state);
+  };
   return (
-    <div className={styles.mainContainer}>
-      <img src="" alt="" />
-      <div>
-        <h2>Contact us</h2>
-        <form action="">
-            <input type="text">Name</input>
-            <input type="email">Email</input>
-            <input type="text" >Message...</input>
-            <Button></Button>
+    <div className={styles.ContactusContainer}>
+      <div className={styles.mainContainer}>
+        <img src={Telefono} alt="" />
+        <div className={styles.formContainer}>
+          <h3>Contact us</h3>
+          <form onSubmit={(e) => HandleSubmit(e)}>
+            <input
+              onChange={HandleChange}
+              type="text"
+              name="name"
+              placeholder="Your name..."
+            />
+            <input
+              onChange={HandleChange}
+              type="email"
+              name="email"
+              placeholder="Email..."
+            />
+            <textarea
+              onChange={HandleChange}
+              name="message"
+              placeholder="Message..."
+            />
+            <Button type="submit" onClick={() => HandleSubmit}>
+              Submit
+            </Button>
             <h6>Or</h6>
-        </form>
-        <div>
-          <button></button>
-          <button></button>
-          <button></button>
+          </form>
+          <div className={styles.AditionalButtonsContainer}>
+            <button className={styles.twitter}>
+              {/* <img src={Twitter} alt="" /> */}
+            </button>
+            <button className={styles.github}>
+              {/*  <img src={GitHub} alt="" /> */}
+            </button>
+            <button className={styles.linkedin}>
+              {/*  <img src={LinkedIn} alt="" /> */}
+            </button>
+          </div>
         </div>
       </div>
     </div>
