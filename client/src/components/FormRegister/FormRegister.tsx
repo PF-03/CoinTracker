12d354f4cf65fd7cf2c1e5944ee7d1eb1aeae93f
AlexSangronis <sangronisalexandra97@gmail.com>
2022-10-31 14:33:48 -0300

    changes-Form-rutaSignUp

--- client/src/components/FormRegister/FormRegister.tsx
+++ client/src/components/FormRegister/FormRegister.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react';
-import axios from 'axios';
-import './FormRegister.css';
-import Button from '../styles/button';
+import { useState } from "react";
+import axios from "axios";
+import "./FormRegister.css";
+import Button from "../styles/button";
+import { useDispatch } from "react-redux";
+import { setUserToken } from "../../redux/actions/index";
+import { useNavigate } from "react-router-dom";
 
 function FormRegister() {
   const [inputs, setInputs] = useState({
-    username: '',
-    password: '',
-    mail: '',
-    name: '',
-    lastname: '',
+    username: "",
+    password: "",
+    mail: "",
+    name: "",
+    lastname: "",
     disabled: true,
     errors: {
-      username: '',
-      password: '',
-      passwordConfirm: '',
-      mail: '',
-      name: '',
-      lastname: '',
+      username: "",
+      password: "",
+      passwordConfirm: "",
+      mail: "",
+      name: "",
+      lastname: "",
     },
   });
 
   function validateForm(errors: Object) {
-    let usernameInput = document.getElementById('username');
-    let mailInput = document.getElementById('mail');
-    let passwordInput = document.getElementById('password');
-    let username = usernameInput.getAttribute('value');
-    let mail = mailInput.getAttribute('value');
-    let password = passwordInput.getAttribute('value');
+    let usernameInput = document.getElementById("username");
+    let mailInput = document.getElementById("mail");
+    let passwordInput = document.getElementById("password");
+    let username = usernameInput.getAttribute("value");
+    let mail = mailInput.getAttribute("value");
+    let password = passwordInput.getAttribute("value");
 
     if (!username || !mail || !password) {
       setInputs({
@@ -60,39 +63,39 @@ function FormRegister() {
 
   function handleChange(e: any) {
     type InputField =
-      | 'name'
-      | 'lastname'
-      | 'username'
-      | 'mail'
-      | 'password'
-      | 'passwordConfirm';
+      | "name"
+      | "lastname"
+      | "username"
+      | "mail"
+      | "password"
+      | "passwordConfirm";
 
     const { value } = e.target;
     const name: InputField = e.target.name;
     let errors = inputs.errors;
 
     switch (name) {
-      case 'name':
+      case "name":
         break;
-      case 'lastname':
+      case "lastname":
         break;
-      case 'username':
-        errors.username = value.length < 5 ? 'Usa minimo 5 letras' : '';
+      case "username":
+        errors.username = value.length < 5 ? "Usa minimo 5 letras" : "";
         break;
-      case 'mail':
+      case "mail":
         let emailPattern = /\S+@\S+\.\S+/; // Expresion Regular para validar Emails.
         errors.mail = !emailPattern.test(value)
-          ? 'Inserta un email valido'
-          : '';
+          ? "Inserta un email valido"
+          : "";
         break;
-      case 'password':
-        errors.password = value.length < 8 ? 'Usa minimo 8 caracteres' : '';
+      case "password":
+        errors.password = value.length < 8 ? "Usa minimo 8 caracteres" : "";
         errors.passwordConfirm =
-          value === inputs.password ? '' : 'Las contraseñas no son iguales';
+          value === inputs.password ? "" : "Las contraseñas no son iguales";
         break;
-      case 'passwordConfirm':
+      case "passwordConfirm":
         errors.passwordConfirm =
-          value === inputs.password ? '' : 'Las contraseñas no son iguales';
+          value === inputs.password ? "" : "Las contraseñas no son iguales";
         break;
       default:
         break;
@@ -105,148 +108,155 @@ function FormRegister() {
       errors,
     });
 
-    if (name !== 'name' && name !== 'lastname') {
+    if (name !== "name" && name !== "lastname") {
       validateForm(inputs.errors);
     }
   }
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   async function submitForm(e: any) {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:3001/localauth/signup', {
-        username: inputs.username,
-        password: inputs.password,
-        mail: inputs.mail,
-        name: inputs.name,
-        lastname: inputs.lastname,
-      });
-      alert('Genial, tu cuenta se creo con éxito.');
-      window.location.href = `/`;
+      await axios
+        .post("http://localhost:3001/localauth/signup", {
+          username: inputs.username,
+          password: inputs.password,
+          mail: inputs.mail,
+          name: inputs.name,
+          lastname: inputs.lastname,
+        })
+        .then((res) => {
+          console.log(res.data.token);
+          dispatch(setUserToken(res.data.token));
+        });
+      alert("Genial, tu cuenta se creo con éxito.");
+      navigate("/home");
     } catch (error) {
       alert(`Ups, algo salio mal intenta de nuevo.\n${error}`);
     }
   }
   const google = () => {
-    window.open('http://localhost:3001/googleauth/google', '_self');
+    window.open("http://localhost:3001/googleauth/google", "_self");
   };
 
   return (
-    <div className='signUpContainer'>
+    <div className="signUpContainer">
       <h2>Crea tu cuenta:</h2>
 
-      <button className='googleSignUp' onClick={google}>
+      <button className="googleSignUp" onClick={google}>
         Registrate con Google
       </button>
 
-      <p className='orText'>O</p>
+      <p className="orText">O</p>
 
-      <form onSubmit={submitForm} className='signUpForm'>
-        <label className='registerFormLabel' htmlFor='name'>
-          Nombre:{' '}
+      <form onSubmit={submitForm} className="signUpForm">
+        <label className="registerFormLabel" htmlFor="name">
+          Nombre:{" "}
         </label>
         <input
-          className='registerFormInput'
-          name='name'
-          id='name'
+          className="registerFormInput"
+          name="name"
+          id="name"
           onChange={handleChange}
-          placeholder='Juan'
+          placeholder="Juan"
         />
         {!inputs.errors.name ? null : (
-          <p className='inputError'>{inputs.errors.name}</p>
+          <p className="inputError">{inputs.errors.name}</p>
         )}
 
-        <label className='registerFormLabel' htmlFor='lastname'>
-          Apellido:{' '}
+        <label className="registerFormLabel" htmlFor="lastname">
+          Apellido:{" "}
         </label>
         <input
-          className='registerFormInput'
-          name='lastname'
-          id='lastname'
+          className="registerFormInput"
+          name="lastname"
+          id="lastname"
           onChange={handleChange}
-          placeholder='Perez'
+          placeholder="Perez"
         />
         {!inputs.errors.lastname ? null : (
-          <p className='inputError'>{inputs.errors.lastname}</p>
+          <p className="inputError">{inputs.errors.lastname}</p>
         )}
 
-        <label className='registerFormLabel' htmlFor='username'>
+        <label className="registerFormLabel" htmlFor="username">
           Usuario:
         </label>
         <input
           required
-          className='registerFormInput'
-          name='username'
-          id='username'
+          className="registerFormInput"
+          name="username"
+          id="username"
           onChange={handleChange}
-          placeholder='Perez'
+          placeholder="Perez"
           value={inputs.username}
         />
         {!inputs.errors.username ? null : (
-          <p className='inputError'>{inputs.errors.username}</p>
+          <p className="inputError">{inputs.errors.username}</p>
         )}
 
-        <label className='registerFormLabel' htmlFor='mail'>
-          Email:{' '}
+        <label className="registerFormLabel" htmlFor="mail">
+          Email:{" "}
         </label>
         <input
           required
-          className='registerFormInput'
-          name='mail'
-          id='mail'
+          className="registerFormInput"
+          name="mail"
+          id="mail"
           onChange={handleChange}
-          placeholder='juanito@gmail.com'
+          placeholder="juanito@gmail.com"
           value={inputs.mail}
         />
         {!inputs.errors.mail ? null : (
-          <p className='inputError'>{inputs.errors.mail}</p>
+          <p className="inputError">{inputs.errors.mail}</p>
         )}
 
-        <label className='registerFormLabel' htmlFor='password'>
-          Contraseña:{' '}
+        <label className="registerFormLabel" htmlFor="password">
+          Contraseña:{" "}
         </label>
         <input
           required
-          className='registerFormInput'
-          type='password'
-          name='password'
-          id='password'
+          className="registerFormInput"
+          type="password"
+          name="password"
+          id="password"
           onChange={handleChange}
-          placeholder=''
+          placeholder=""
           value={inputs.password}
         />
         {!inputs.errors.password ? null : (
-          <p className='inputError'>{inputs.errors.password}</p>
+          <p className="inputError">{inputs.errors.password}</p>
         )}
 
-        <label className='registerFormLabel' htmlFor='passwordConfirm'>
-          Confirma contraseña:{' '}
+        <label className="registerFormLabel" htmlFor="passwordConfirm">
+          Confirma contraseña:{" "}
         </label>
         <input
-          className='registerFormInput'
-          type='password'
-          name='passwordConfirm'
-          id='passwordConfirm'
+          className="registerFormInput"
+          type="password"
+          name="passwordConfirm"
+          id="passwordConfirm"
           onChange={handleChange}
-          placeholder=''
+          placeholder=""
         />
         {!inputs.errors.passwordConfirm ? null : (
-          <p className='inputError'>{inputs.errors.passwordConfirm}</p>
+          <p className="inputError">{inputs.errors.passwordConfirm}</p>
         )}
 
         {/* <input className='submit-btn' id='submit-btn' type="submit" value='Crear cuenta' disabled={inputs.disabled}/> */}
         <Button
           gradient={inputs.disabled ? false : true}
-          className='submit-btn'
-          id='submit-btn'
-          type='submit'
+          className="submit-btn"
+          id="submit-btn"
+          type="submit"
           disabled={inputs.disabled}
         >
           Crear cuenta
         </Button>
       </form>
 
-      <p className='loginText'>
-        ¿Ya tienes una cuenta? <a href=''>Haz login</a>
+      <p className="loginText">
+        ¿Ya tienes una cuenta? <a href="">Haz login</a>
       </p>
     </div>
   );
--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,31 +1,32 @@
-import style from './Sidebar.module.css';
-import card from '../styles/styles.module.css';
-import { Link } from 'react-router-dom';
-import iconHome from '../../assets/iconHome.png';
-import iconCal from '../../assets/iconCal.png';
-import iconLogOut from '../../assets/iconLogOut.png';
-import iconNew from '../../assets/iconNews.png';
-import iconPor from '../../assets/iconPortfolio.png';
-import iconSetting from '../../assets/iconSettings.png';
-import iconSwap from '../../assets/iconSwap.png';
-import iconUser from '../../assets/iconUser.png';
-import { useSelector } from 'react-redux';
+import style from "./Sidebar.module.css";
+import card from "../styles/styles.module.css";
+import { Link } from "react-router-dom";
+import iconHome from "../../assets/iconHome.png";
+import iconCal from "../../assets/iconCal.png";
+import iconLogOut from "../../assets/iconLogOut.png";
+import iconNew from "../../assets/iconNews.png";
+import iconPor from "../../assets/iconPortfolio.png";
+import iconSetting from "../../assets/iconSettings.png";
+import iconSwap from "../../assets/iconSwap.png";
+import iconUser from "../../assets/iconUser.png";
+import { useSelector } from "react-redux";
 
 // codigo guglielmo
 ////////////////////////////////////////////////////////
-import { setUser } from '../../redux/actions';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import axios from 'axios';
+import { setUser } from "../../redux/actions";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
 //////////////////////////////////////////////////////////
 function Sidebar() {
   const user = useSelector((state: any) => state.user); // para el user del readux, cambiar en caso de ser necesario
-
+  console.log(user);
   //codigo guglielmo
   ////////////////////////////////////////////////////////////
 
   const dispatch = useDispatch();
   const token = useSelector((store: any) => store.userToken);
+  console.log(token);
 
   const logout = () => {
     axios
@@ -62,48 +63,48 @@ function Sidebar() {
     <div className={`${style.sidebar} ${card.card}`}>
       <div className={style.head}>
         <img
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Ethereum_logo_translucent.svg/640px-Ethereum_logo_translucent.svg.png'
-          alt='logo'
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Ethereum_logo_translucent.svg/640px-Ethereum_logo_translucent.svg.png"
+          alt="logo"
           className={style.logo}
         />
-        <Link className={style.title} to='/home'>
-          {' '}
+        <Link className={style.title} to="/home">
+          {" "}
           CoinTracker
         </Link>
       </div>
       <div className={style.options}>
-        <Link to='/home' className={style.data}>
-          <img className={style.icon} src={iconUser} alt='user' />
-          <span>{user.name || 'Default'}</span>
+        <Link to="/home" className={style.data}>
+          <img className={style.icon} src={iconUser} alt="user" />
+          <span>{user.name || "Default"}</span>
         </Link>
-        <Link to='/home' className={style.data}>
-          <img className={style.icon} src={iconHome} alt='home' />
+        <Link to="/home" className={style.data}>
+          <img className={style.icon} src={iconHome} alt="home" />
           <span>Home</span>
         </Link>
-        <Link to='/porfolio' className={style.data}>
-          <img className={style.icon} src={iconPor} alt='home' />
+        <Link to="/porfolio" className={style.data}>
+          <img className={style.icon} src={iconPor} alt="home" />
           <span>Portfolio Tracker</span>
         </Link>
-        <Link to='/news' className={style.data}>
-          <img className={style.icon} src={iconNew} alt='home' />
+        <Link to="/news" className={style.data}>
+          <img className={style.icon} src={iconNew} alt="home" />
           <span>News</span>
         </Link>
-        <Link to='/swap' className={style.data}>
-          <img className={style.icon} src={iconSwap} alt='home' />
+        <Link to="/swap" className={style.data}>
+          <img className={style.icon} src={iconSwap} alt="home" />
           <span>Swap</span>
         </Link>
-        <Link to='/calculator' className={style.data}>
-          <img className={style.icon} src={iconCal} alt='home' />
+        <Link to="/calculator" className={style.data}>
+          <img className={style.icon} src={iconCal} alt="home" />
           <span>Calculator</span>
         </Link>
       </div>
       <div className={style.footer}>
-        <Link to='' className={style.data}>
-          <img className={style.icon} src={iconSetting} alt='home' />
+        <Link to="" className={style.data}>
+          <img className={style.icon} src={iconSetting} alt="home" />
           <span>Settings</span>
         </Link>
-        <Link to='/' onClick={() => logout()} className={style.data}>
-          <img className={style.icon} src={iconLogOut} alt='home' />
+        <Link to="/" onClick={() => logout()} className={style.data}>
+          <img className={style.icon} src={iconLogOut} alt="home" />
           <span>Logout</span>
         </Link>
       </div>
