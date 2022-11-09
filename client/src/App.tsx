dfdff1818211629ecf09d5193b5e2d62dbf09cf8
AlexSangronis <sangronisalexandra97@gmail.com>
2022-11-09 15:21:58 -0300

    Chat-Socket-Bot

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-
 import AuthGuard from "./guard/auth.guard";
 import { PrivateAdminRoutes, PrivateRoutes, PublicRouts } from "./rutas/rutas";
 import { AdminGuard } from "./guard/admin.guard";
@@ -12,6 +11,7 @@ import Calculadora from "./components/Calculadora/calculadora";
 import AdminView from "./components/admin/AdminView";
 import SharedLayout from "./views/sharedLayout/SharedLayout";
 import Donation from "./components/Donation/donation";
+import Chatbot from "./components/ChatBot/chatBots";
 
 import SwapComponent from "./components/swapComponent/SwapComponent";
 
@@ -26,11 +26,11 @@ const Review = lazy(() => import("./components/Review/Review"));
 const Home = lazy(() => import("./views/home/home"));
 // const Calculator = lazy(() => import("./components/Calculadora/calculadora"));
 const FormRegister = lazy(() => import("./views/FormRegister/FormRegister"));
-const DetailsActivs = lazy(
-  () => import("./components/details-activs/detailsActivs")
+const DetailsActivs = lazy(() =>
+  import("./components/details-activs/detailsActivs")
 );
-const Activos = lazy(
-  () => import("./components/ver_mas_activos/ver_mas_activos")
+const Activos = lazy(() =>
+  import("./components/ver_mas_activos/ver_mas_activos")
 );
 
 function App() {
@@ -50,6 +50,7 @@ function App() {
           <Route path={PrivateRoutes.REVIEW} element={<Review />} />
 
           <Route path={PrivateRoutes.VERIFIQUED} element={<Verifiqued />} />
+          <Route path="/chat" element={<Chatbot />} />
 
           <Route element={<SharedLayout />}>
             <Route path={PrivateRoutes.CRYPTO} element={<DetailsActivs />} />
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,58 +1,87 @@
-require('dotenv').config();
-require('./auth/googleAuth');
-require('./auth/localAuth');
-import { Request, Response, NextFunction } from 'express';
-import express from 'express';
-import morgan from 'morgan';
-import routers from './routes/index';
-import { dbConn } from './db';
-import cors from 'cors';
-import session from 'express-session';
-import passport from 'passport';
-import cookieParser from 'cookie-parser';
-import multer from 'multer';
-import path from 'path';
-import { ActualizarApi } from './controllers/actives';
-
+require("dotenv").config();
+require("./auth/googleAuth");
+require("./auth/localAuth");
+import { Request, Response, NextFunction } from "express";
+import express from "express";
+import morgan from "morgan";
+import routers from "./routes/index";
+import { dbConn } from "./db";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import path from "path";
+import { ActualizarApi } from "./controllers/actives";
 const app: any = express();
 
+//Conectamos socket
+import { Server as SocketServer } from "socket.io";
+let http = require("http");
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    credentials: true,
+  },
+});
+///
+
 //Middlewares
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 app.use(
-  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 })
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
 );
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
   })
 );
 app.use(
   session({
-    secret: 'secreto',
+    secret: "secreto",
     resave: true,
     saveUninitialized: true,
   })
 );
-app.use(cookieParser('secreto'));
+app.use(cookieParser("secreto"));
 app.use(passport.initialize());
 app.use(passport.session());
+
+//conectamos socket io para que escuche
+
+io.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    socket.emit("message", {
+      body: message.body,
+      from: message.from === "User" ? "me" : message.from,
+    });
+    socket.broadcast.emit("message", {
+      body: message.body,
+      from: message.from,
+    });
+  });
+  socket.on("typing", (usuario) => {
+    socket.broadcast.emit("typing", usuario + " is typing...");
+  });
+});
 
 //para que entienda los datos que le enviamos
 
 //multer
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, 'public/img'),
+  destination: path.join(__dirname, "public/img"),
   filename: (req: any, file: any, cb: any) => {
     cb(null, new Date().getTime() + path.extname(file.originalname));
   },
 });
-app.use(multer({ storage }).single('image'));
+app.use(multer({ storage }).single("image"));
 
 // add exchange history routes
-app.use('/', routers);
+app.use("/", routers);
 
 // Error catching endware.
 app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
@@ -69,14 +98,14 @@ function repetir() {
 }
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   // puerto 3001
   repetir();
-  console.log('Server listening on port 3001'); // eslint-disable-line no-console
+  console.log("Server listening on port 3001"); // eslint-disable-line no-console
 });
 
 //Conectamos a la base de datos
 dbConn();
 function _(err: any): void {
-  throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
