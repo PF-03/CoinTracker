553003c1b08850167ab434a6e8e74e2868fba88b
Guglielmo Caliendo <guglielmocaliendo92@gmail.com>
2022-11-14 17:44:18 -0400

    fix carousel

--- client/src/components/carousel-activs/carousel.tsx
+++ client/src/components/carousel-activs/carousel.tsx
@@ -1,23 +1,21 @@
-import { Navigation, A11y } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { useSelector, useDispatch } from "react-redux/es/exports";
-import { useEffect } from "react";
-import { getActivos } from "../../redux/actions";
+import { Navigation, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { useEffect } from 'react';
+import { getActivos } from '../../redux/actions';
 // Import Swiper styles import 'swiper/css';
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 /*  import 'swiper/css/scrollbar';  */
-import style from "./carousel.module.css";
-import Card from "../card/card";
-import numberFormat from "../../utils/numberFormat.js";
-
+import style from './carousel.module.css';
+import Card from '../card/card';
+import numberFormat from '../../utils/numberFormat.js';
 
 export default function Carousel() {
   const dispatch: any = useDispatch();
   const activos = useSelector((state: any) => state.allactivos);
   const see = useSelector((state: any) => state.seeMore);
-  const activosSlice = activos.slice(0, 6);
-  console.log(activos);
+  const activosSlice = activos.slice(0, 8);
   useEffect(() => {
     dispatch(getActivos());
   }, [dispatch]);
@@ -29,27 +27,27 @@ export default function Carousel() {
           <Swiper
             // install Swiper modules
             modules={[Navigation, /* Scrollbar */ A11y]}
-            slidesOffsetAfter={50}
+            slidesOffsetAfter={10}
             slidesPerView={3.5}
             navigation
             scrollbar={{ draggable: true }}
           >
             {activosSlice.length > 0 &&
-              activosSlice !== "error" &&
+              activosSlice !== 'error' &&
               activosSlice?.map((el: coinInfo) => (
                 <SwiperSlide key={el.name}>
                   <Card
                     id={el.id}
                     name={el.name}
                     image={el.image}
-
-                    current_price={numberFormat(el.current_price, 'standard', 'decimal')}
-
+                    current_price={numberFormat(
+                      el.current_price,
+                      'standard',
+                      'decimal'
+                    )}
                   />
                 </SwiperSlide>
               ))}
-            <SwiperSlide>
-            </SwiperSlide>
           </Swiper>
         </div>
       </div>
